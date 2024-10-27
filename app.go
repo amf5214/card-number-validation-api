858c2b2592007a00727b43aa8a3f72f963d1e657
amf5214 <austinfraley.lee@gmail.com>
2024-10-27 16:13:41 -0400

    Implemented card number validation path
    Implemented card number validation path using custom function that implements the Luhn algorithm

--- app.go
+++ app.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	cardvalidation "card-validator/card-validation"
-	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -13,15 +12,42 @@ type card struct {
 }
 
 func main() {
-	fmt.Print(cardvalidation.ValidateCardNumber("5122301102209636"))
+	// Configure the api router
 	router := gin.Default()
 
+	// Configure Status path
 	router.GET("/status", getStatus)
 
+	// Configure validation path
+	router.PUT("/validate", putCardNumber)
+
+	// Run server
 	router.Run("localhost:8080")
 }
 
+// Status path to check the status of the api
 func getStatus(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, "Operating Normally")
 }
+
+// validate credit card number using custom function
+func putCardNumber(c *gin.Context) {
+	var newCard card
+
+	// Call BindJSON to bind the received JSON to newCard
+	if err := c.BindJSON(&newCard); err != nil {
+		c.IndentedJSON(http.StatusBadRequest, "Invalid Request")
+		return
+	}
+
+	// Validate card number using custom function
+	isValid := cardvalidation.ValidateCardNumber(newCard.CardNumber)
+
+	// Send custom response based on whether or not the card number is valid
+	if isValid {
+		c.IndentedJSON(http.StatusOK, "Card number is valid")
+	} else {
+		c.IndentedJSON(http.StatusOK, "Card number is invalid")
+	}
+
 }
