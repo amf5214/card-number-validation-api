37f0b55d7366442676f0ddda6d55880ad2af5bc3
amf5214 <austinfraley.lee@gmail.com>
2024-10-27 16:01:27 -0400

    Added API Structure and added a test function
    Added API Structure and added a test function

--- app.go
+++ app.go
@@ -3,6 +3,9 @@ package main
 import (
 	cardvalidation "card-validator/card-validation"
 	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
 
 type card struct {
@@ -11,4 +14,14 @@ type card struct {
 
 func main() {
 	fmt.Print(cardvalidation.ValidateCardNumber("5122301102209636"))
+	router := gin.Default()
+
+	router.GET("/status", getStatus)
+
+	router.Run("localhost:8080")
+}
+
+func getStatus(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, "Operating Normally")
+}
 }
